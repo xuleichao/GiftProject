56d896aa5f001aa9db90bd8406938555d3032c5c
xuleichao <xuleichao@1yongcloud.com>
2019-06-13 09:18:34 +0800

    new-branch

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/egg')
 def hello_world():
     template = render_template('sign_in.html')
     return template
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
